c99fd9f21829590e2800202f7a7334a7c148b28b
ariefyudopradana <50146464+ariefyudopradana@users.noreply.github.com>
2019-05-11 19:10:02 +0700

    Add files via upload

---
+++ data/Kesehatan_31.js
@@ -0,0 +1,10 @@
+var json_Kesehatan_31 = {
+"type": "FeatureCollection",
+"name": "Kesehatan_31",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Nama": "Rumah Sakit Baptis Batu", "Koordinat": "112°32'15.88\"E,7°54'26.56\"S", "Lokasi": "Tlekung", "Kodepos": 65327.0 }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 112.537540087000025, -7.907073903999958, 0.0 ] ] } },
+{ "type": "Feature", "properties": { "Nama": "Pustu Junrejo - Puskesmas Junrejo", "Koordinat": "112°33'36.93\"E,7°54'39.94\"S", "Lokasi": "Junrejo", "Kodepos": 65321.0 }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 112.560063993000085, -7.910941998999931, 0.0 ] ] } },
+{ "type": "Feature", "properties": { "Nama": "Puskesmas Beji", "Koordinat": "112°32'37.56\"E,7°53'17.20\"S", "Lokasi": "Beji", "Kodepos": 65326.0 }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 112.543546788000072, -7.888138965999929, 0.0 ] ] } }
+]
+}
---
+++ data/Pendidikan_30.js
@@ -0,0 +1,25 @@
+var json_Pendidikan_30 = {
+"type": "FeatureCollection",
+"name": "Pendidikan_30",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Nama": "MI Iskandar Sulaiman", "Koordinat": "112°34'57.65\"E,7°53'35.37\"S", "Lokasi": "Pendem", "Kodepos": "65324" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 112.58263313000009, -7.893203574999973, 0.0 ] ] } },
+{ "type": "Feature", "properties": { "Nama": "SDN Pendem 01", "Koordinat": "112°34'57.65\"E,7°54'06.81\"S", "Lokasi": "Pendem", "Kodepos": "65324" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 112.582575148000046, -7.901878813999929, 0.0 ] ] } },
+{ "type": "Feature", "properties": { "Nama": "SDN Pendem 02", "Koordinat": "112°34'50.97\"E,7°54'10.12\"S", "Lokasi": "Pendem", "Kodepos": "65324" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 112.580647366000107, -7.90277365299994, 0.0 ] ] } },
+{ "type": "Feature", "properties": { "Nama": null, "Koordinat": null, "Lokasi": null, "Kodepos": null }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 112.536203446000059, -7.911466029999929, 0.0 ] ] } },
+{ "type": "Feature", "properties": { "Nama": null, "Koordinat": null, "Lokasi": null, "Kodepos": null }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 112.535472915000014, -7.905377011999974, 0.0 ] ] } },
+{ "type": "Feature", "properties": { "Nama": "SMP Al Irsyad", "Koordinat": "112°33'10.06\"E,7°54'29.65\"S", "Lokasi": "Mojorejo", "Kodepos": "65322" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 112.559118602000012, -7.899887091999972, 0.0 ] ] } },
+{ "type": "Feature", "properties": { "Nama": "SDN Junrejo 02", "Koordinat": "112°33'23.76\"E,7°54'40.57\"S", "Lokasi": "Junrejo", "Kodepos": "65321" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 112.55664958300008, -7.911239230999968, 0.0 ] ] } },
+{ "type": "Feature", "properties": { "Nama": "SDN Junrejo 01", "Koordinat": "112°33'09.40\"E,7°54'30.31\"S", "Lokasi": "Junrejo", "Kodepos": "65321" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 112.55266519700001, -7.908220795999966, 0.0 ] ] } },
+{ "type": "Feature", "properties": { "Nama": "SDN Torongrejo 2", "Koordinat": "112°34'10.54\"E,7°53'35.37\"S", "Lokasi": "Torongrejo", "Kodepos": "65325" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 112.56968324200011, -7.893151558999932, 0.0 ] ] } },
+{ "type": "Feature", "properties": { "Nama": "SDN Torongrejo 1", "Koordinat": "112°33'32.78\"E,7°53'18.16\"S", "Lokasi": "Torongrejo", "Kodepos": "65325" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 112.558933804000048, -7.888067449999937, 0.0 ] ] } },
+{ "type": "Feature", "properties": { "Nama": "Sekolah Dasar Negeri Temas 02", "Koordinat": null, "Lokasi": "Beji", "Kodepos": "65326" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 112.553952869000113, -7.895547028999943, 0.0 ] ] } },
+{ "type": "Feature", "properties": { "Nama": "SDN Torongrejo 3", "Koordinat": "112°33'15.08\"E,7°52'42.42\"S", "Lokasi": "Torongrejo", "Kodepos": "65325" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 112.554169054000113, -7.878337451999926, 0.0 ] ] } },
+{ "type": "Feature", "properties": { "Nama": "SDN Mojorejo 01", "Koordinat": "112°34'08.87\"E,7°54'21.04\"S", "Lokasi": "Mojorejo", "Kodepos": "65322" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 112.56899433600006, -7.90555879599998, 0.0 ] ] } },
+{ "type": "Feature", "properties": { "Nama": "SMP Negeri 3 Batu", "Koordinat": "112°32'38.66\"E,7°53'19.16\"S", "Lokasi": "Beji", "Kodepos": "65326" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 112.543996820000075, -7.888538184999959, 0.0 ] ] } },
+{ "type": "Feature", "properties": { "Nama": "Batu Bible School", "Koordinat": "112°32'48.35\"E,7°53'20.48\"S", "Lokasi": "Beji", "Kodepos": "65326" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 112.546535258000063, -7.889151546999926, 0.0 ] ] } },
+{ "type": "Feature", "properties": { "Nama": "SDN Beji 01", "Koordinat": "112°32'52.36\"E,7°53'29.75\"S", "Lokasi": "Beji", "Kodepos": "65326" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 112.547823053000116, -7.891558092999958, 0.0 ] ] } },
+{ "type": "Feature", "properties": { "Nama": "SDN Beji 02", "Koordinat": "112°32'52.36\"E,7°53'41.99\"S", "Lokasi": "Beji", "Kodepos": "65326" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 112.547786149000103, -7.895135124999968, 0.0 ] ] } },
+{ "type": "Feature", "properties": { "Nama": "MTS Negeri Kota Batu", "Koordinat": null, "Lokasi": "Dadaprejo", "Kodepos": "65323" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 112.5806086340001, -7.911188484999968, 0.0 ] ] } }
+]
+}
